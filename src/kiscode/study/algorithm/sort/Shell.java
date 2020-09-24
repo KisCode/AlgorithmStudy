dc7db52428794062ef6aadfb66629ba1539f5007
keno <keno123@126.com>
2020-09-24 23:06:45 +0800

    opt Shell sort

--- src/kiscode/study/algorithm/sort/Shell.java
+++ src/kiscode/study/algorithm/sort/Shell.java
@@ -24,6 +24,14 @@ public class Shell {
                 }
             }*/
 
+            for (int i = 0; i < length - gap; i++) {
+                for (int j = i + gap; j > 0; j -= gap) {
+                    if (SortUtil.greater(arr[j - 1], arr[j])) {
+                        SortUtil.exchange(arr, j - 1, j);
+                    }
+                }
+            }
+/*
             //1. 找到待插入的元素（有序数组）
             for (int i = gap; i < length; i++) {
                 // 把待插入的元素插入到有序数组中
@@ -32,7 +40,7 @@ public class Shell {
                         SortUtil.exchange(arr, j - gap, j);
                     }
                 }
-            }
+            }*/
             gap = gap / 2;
         }
     }
