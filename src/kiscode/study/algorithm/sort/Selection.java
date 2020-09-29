22c0e7b0e85ab17d089b78e68910bc8d37f2d203
keno <keno123@126.com>
2020-09-29 22:08:05 +0800

    add comment about Selection sort ;

--- src/kiscode/study/algorithm/sort/Selection.java
+++ src/kiscode/study/algorithm/sort/Selection.java
@@ -4,11 +4,18 @@ import kiscode.study.algorithm.sort.util.SortUtil;
 
 /****
  * Description:选择排序
+ *优点：相比冒泡排序交换次数少
  * Author:  keno
  * CreateDate: 2020/9/23 21:52
  */
 public class Selection {
 
+    /***
+     * 选择排序
+     * 1. 依次将每个元素和数组中其他元素进行比较，记录最小值的下标，并进行交换
+     * 2. 下一个元素 继续第1步
+     * @param arr 待排序数组
+     */
     public static void sort(Comparable[] arr) {
         int minIndex;
         for (int i = 0; i < arr.length; i++) {
